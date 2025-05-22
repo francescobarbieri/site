9e158f95d2efcf799b8f74effbc6271d69e37552
Francesco Barbieri <fbarbieri.business@gmail.com>
2025-05-22 18:11:02 +0200

    sitemap integratin

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 
+import sitemap from "@astrojs/sitemap";
+
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.barbierinotes.com",
@@ -13,5 +16,12 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [mdx()]
+  integrations: [
+    mdx(), 
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
 });

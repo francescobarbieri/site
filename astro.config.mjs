3b203838786d78b898170780d981090c6791b16d
Francesco Barbieri <fbarbieri.business@gmail.com>
2025-05-26 11:18:20 +0200

    added all fonts for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-// @ts-check
 import mdx from '@astrojs/mdx';
 import { defineConfig } from 'astro/config';
 import vercelStatic from "@astrojs/vercel/static";
 
-
-// https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercelStatic({
